aaa46f4c8bb14c12649bd5a43ffeaa26ef3d68f2
Miyama1209 <samcsph@gmail.com>
2019-01-29 17:52:17 +0800

    not change

--- koudou.R
+++ koudou.R
@@ -50,3 +50,128 @@ lm(df$y ~ df$x)
 a <- "Hello"
 b <- "World"
 rm(a,b)
+
+#dplyr box
+mpg %>%
+  filter(manufacturer=="audi") %>%
+  group_by(model) %>%
+  summarise(hwy_mean = mean(hwy))
+
+#filter: select rows
+starwars %>%
+  filter(species == "Human", height >= 190)
+
+starwars %>%
+  filter(grepl("Skywalker", name))
+
+starwars %>%
+  filter(is.na(height))
+
+starwars %>%
+  filter(!is.na(height))
+
+#arrange: reorder
+starwars %>%
+  arrange(birth_year)
+
+starwars %>%
+  arrange(desc(birth_year))
+
+#select: columns?
+starwars %>%
+  select(name:skin_color, species, -height)
+
+starwars %>%
+  select(alias=name, crib=homeworld, sex=gender)
+
+starwars %>%
+  select(name, contains("color"))
+
+starwars %>%
+  select(species, homeworld, everything())
+
+#mutate: create new columns
+starwars %>%
+  select(name, birth_year) %>%
+  mutate(dog_years = birth_year * 7) %>%
+  mutate(comment = paste0(name, " is ", dog_years, " in dog years."))
+
+starwars %>%
+  select(name, birth_year) %>%
+  mutate(dog_years = birth_year * 7, comment = paste0(name, " is ", dog_years, " in dog years."))
+
+starwars %>%
+  select(name, height) %>%
+  filter(name %in% c("Luke Skywalker", "Anakin Skywalker")) %>%
+  mutate(tall1 = height > 180) %>%
+  mutate(tall2 = ifelse(height > 180, "Tall", "Short"))
+
+starwars %>%
+  group_by(species, gender) %>%
+  summarise(mean_height = mean(height, na.rm = T))
+
+starwars %>%
+  group_by(species, gender) %>%
+  summarise_if(is.numeric, funs(avg=mean), na.rm=T) %>%
+  head(5)
+
+starwars %>% slice(c(1,5))#subset rows by position
+
+starwars %>% filter(gender=="female") %>% pull(height)
+ 
+starwars %>% count(species)
+
+fun <- starwars %>% group_by(species) %>% mutate(num = n())
+
+library(nycflights13)
+flights
+planes
+
+left_join(flights, planes) %>%
+  select(year, month, day, dep_time, arr_time, carrier, flight, tailnum, type, model)
+
+left_join(flights, planes %>% rename(year_built=year), by="tailnum" ) %>%
+  select(year, month, day, dep_time, arr_time, carrier, flight, tailnum, year_built)
+
+left_join(flights,  planes, by = "tailnum") %>%
+  select(contains("year"), month, day, dep_time, arr_time, carrier, flight, tailnum, type, model) %>%
+  head(3)
+
+#tidyr box
+
+stocks <- data.frame(time = as.Date('2009-01-01') + 0:1,
+                     X=rnorm(2, 0, 1),
+                     Y=rnorm(2, 0, 2),
+                     Z=rnorm(2, 0, 4))
+stocks
+
+tidy_stocks <- stocks %>% gather(key=stock, value=price, -time)
+tidy_stocks
+
+tidy_stocks %>% spread(stock, price)
+tidy_stocks %>% spread(time, price)
+
+economists <- data.frame(name = c("Adam.Smith", "Paul.Samuelson", "Milton.Friedman"))
+
+economists %>% separate(name, 
+                        c("first_name", "last_name"),
+                        sep=".")
+
+jobs <- data.frame(
+  name = c("Jack", "Jill"),
+  occupation = c("Homemaker", "Philosopher, Philanthropist, Troublemaker") 
+) 
+jobs
+
+jobs %>% separate_rows(occupation)
+
+gdp <- data.frame(yr=rep(2016, times=4),
+                  mnth=rep(1, times=4),
+                  dy=1:4,
+                  gdp=rnorm(4, mean=100, sd=2))
+gdp
+gdp %>% unite(date, c("yr", "mnth", "dy"), sep="-")
+gdp_u <- gdp %>% unite(date, c("yr", "mnth", "dy"), sep="-") %>% as_tibble()
+gdp_u
+library(lubridate)
+gdp_u %>% mutate(date = ymd(date))
